3c99a5ba669628f119ac63be361147c3017f4fb4
Balazs Kereszturi <19balazs86@gmail.com>
2024-07-23 18:42:24 +0200

    Comment out: Configuration.Consul

--- Playing-with-Serilog/Program.cs
+++ Playing-with-Serilog/Program.cs
@@ -1,70 +1,71 @@
 ﻿using Serilog;
 using Serilog.Events;
-using Winton.Extensions.Configuration.Consul;
 
-namespace Playing_with_Serilog
+namespace Playing_with_Serilog;
+
+public static class Program
 {
-  public static class Program
-  {
     public static bool UsingConsul => false;
 
     public static void Main(string[] args)
     {
-      IHostBuilder hostBuilder;
+        IHostBuilder hostBuilder;
 
-      IConfiguration config = new ConfigurationBuilder()
-        .AddJsonFile("hosting.json", optional: true)
-        //.AddEnvironmentVariables() if you need
-        .AddCommandLine(args)
-        .Build();
+        IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("hosting.json", optional: true)
+            //.AddEnvironmentVariables() if you need
+            .AddCommandLine(args)
+            .Build();
 
-      hostBuilder = createHostBuilder(config);
+        hostBuilder = createHostBuilder(config);
 
-      hostBuilder.Build().Run();
+        hostBuilder.Build().Run();
     }
 
     private static IHostBuilder createHostBuilder(IConfiguration config)
     {
-      return Host
-        .CreateDefaultBuilder()
-        .ConfigureWebHostDefaults(webHostBuilder =>
-        {
-          webHostBuilder
-           .UseConfiguration(config)
-           .UseStartup<Startup>()
-           .ConfigureAppConfiguration((hostContext, configBuilder) =>
-           {
-             string environmentName = hostContext.HostingEnvironment.EnvironmentName;
+        return Host
+            .CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                webHostBuilder
+                .UseConfiguration(config)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostContext, configBuilder) =>
+                {
+                    string environmentName = hostContext.HostingEnvironment.EnvironmentName;
 
-             if (UsingConsul)
-             {
-               configBuilder.AddConsul($"App1/appsettings.{environmentName}.json", options =>
-               {
-                 // You won't get any exceptions, if it is optional and ignore exception.
+                    if (UsingConsul)
+                    {
+                         // Nuget: Winton.Extensions.Configuration.Consul
+                         //configBuilder.AddConsul($"App1/appsettings.{environmentName}.json", options =>
+                         //{
+                         //  // You won't get any exceptions, if it is optional and ignore exception.
 
-                 options.ConsulConfigurationOptions = cco => cco.Address = new Uri("http://localhost:8500");
-                 options.Optional = false;
-                 options.ReloadOnChange = false;
-                 options.OnLoadException = exceptionContext => exceptionContext.Ignore = false;
-               });
-             }
-           });
-        })
-        //.UseSerilog(configureSerilog1)
-        .UseSerilog();
+                         //  options.ConsulConfigurationOptions = cco => cco.Address = new Uri("http://localhost:8500");
+                         //  options.Optional = false;
+                         //  options.ReloadOnChange = false;
+                         //  options.OnLoadException = exceptionContext => exceptionContext.Ignore = false;
+                         //});
+                    }
+                });
+            })
+            //.UseSerilog(configureSerilog1)
+            .UseSerilog();
     }
 
     private static void configureSerilog1(HostBuilderContext context, LoggerConfiguration configuration)
-      => configuration.ReadFrom.Configuration(context.Configuration);
+    {
+        configuration.ReadFrom.Configuration(context.Configuration);
+    }
 
     private static void configureSerilog2(HostBuilderContext context, LoggerConfiguration configuration)
     {
-      configuration
-        .MinimumLevel.Debug()
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
-        .MinimumLevel.Override("System", LogEventLevel.Error)
-        .Enrich.FromLogContext()
-        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {Message}{NewLine}{Exception}");
+        configuration
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+            .MinimumLevel.Override("System", LogEventLevel.Error)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {Message}{NewLine}{Exception}");
     }
-  }
 }
